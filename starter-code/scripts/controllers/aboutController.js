3604508922f7f43bbc1ba3f2eb32e1378b5c7462
Rick Patci <rpatci@gmail.com>
2016-05-22 23:30:28 -0700

    push starter-code for class-11

---
+++ starter-code/scripts/controllers/aboutController.js
@@ -0,0 +1,9 @@
+(function(module) {
+  var aboutController = {};
+
+  aboutController.index = function() {
+    /* TODO: We only want to display the about section! */
+  };
+
+  module.aboutController = aboutController;
+})(window);
---
+++ starter-code/scripts/controllers/articleController.js
@@ -0,0 +1,16 @@
+(function(module) {
+  var articlesController = {};
+
+  /* TODO: Try and initialize our database table when the controller
+      first loads */
+
+  articlesController.index = function() {
+    /* TODO: Complete this function that kicks off the fetching and
+        rendering of articles. What will happen when a user navigates
+        back and forth between home and about?
+
+    /* TODO: Also be sure to reveal only the articles section */
+  };
+
+  module.articlesController = articlesController;
+})(window);
