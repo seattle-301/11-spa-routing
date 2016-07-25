187144778bff9b17d7875b9620d5f35e21520e57
Rick Patci <rpatci@gmail.com>
2016-07-25 16:31:54 -0700

    push lecture code

--- starter-code/scripts/controllers/routes.js
+++ starter-code/scripts/controllers/routes.js
@@ -2,5 +2,6 @@
     by registering each URL your app can handle, linked to a
     single controller function to handle it: */
 
-/* TODO: What function do we call to activate page.js?
+/* TODO: --DONE!-- What function do we call to activate page.js?
     Fire it off now, to execute it: */
+page();
